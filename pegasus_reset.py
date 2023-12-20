c0460548f5ebb60101b65ea98e6c7f4a35a3bfcf
Lorenzo Buffoni <lbuffoni@lx.it.pt>
2023-12-20 11:35:27 +0100

    pegasus fast protocol

--- pegasus_reset.py
+++ pegasus_reset.py
@@ -23,8 +23,8 @@ total_schedules = []
 
 #h_schedules.append([[0, 0], [10, 0], [20, 1], [anneal_lenght, 0], [anneal_lenght + 0.5, 0]])
 #total_schedules.append([[0, 1], [10, 1 - hmax], [20, 1 - hmax], [anneal_lenght, 1], [anneal_lenght + 0.5, 1]])
-h_schedules.append([[0, 0], [10, 0], [20, 1], [20.1, 0]])
-total_schedules.append([[0, 1], [10, 1 - hmax], [20, 1 - hmax], [20.1, 1]])
+h_schedules.append([[0, 0], [10, 0], [20, 1], [20.3, 0]])
+total_schedules.append([[0, 1], [10, 1 - hmax], [20, 1 - hmax], [20.3, 1]])
 
 
 
