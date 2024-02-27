c17b52fc40679a1aa8b02f47266892b5589e439b
Lorenzo Buffoni <lbuffoni@lx.it.pt>
2024-02-27 13:28:36 +0100

    internal baseline

--- pegasus_reset.py
+++ pegasus_reset.py
@@ -4,34 +4,36 @@ import dimod
 from dwave.system import DWaveSampler
 
 
-mytoken = 'DEV-2942a9351f40088a2e32f4f1732b5dd8dcffea46' # michele
-#mytoken = 'CINE-7a7dd30e6b6196bae3c9c198ee323b7e2ea3f2ed'
+#mytoken = 'DEV-2942a9351f40088a2e32f4f1732b5dd8dcffea46' # michele
+mytoken = 'CINE-7a7dd30e6b6196bae3c9c198ee323b7e2ea3f2ed'
 solver = 'Advantage_system6.3'
 
 qpu_sampler = DWaveSampler(solver=solver, token=mytoken)
 nval = len(qpu_sampler.edgelist)
 
 #hvals = np.linspace(0,2,20)
-hvals = [2.]
-hmax = 0.6
+hvals = [0.] #2.0
+hmax = 0.65
 
-num_samples = 200  # to be multiplied by num_reads
-anneal_lenght = 20.1  # microseconds
+num_samples = 1000  # to be multiplied by num_reads
+anneal_lenght = 30.1  # microseconds
 
 h_schedules = []
 total_schedules = []
 
 #h_schedules.append([[0, 0], [10, 0], [20, 1], [anneal_lenght, 0], [anneal_lenght + 0.5, 0]])
 #total_schedules.append([[0, 1], [10, 1 - hmax], [20, 1 - hmax], [anneal_lenght, 1], [anneal_lenght + 0.5, 1]])
-h_schedules.append([[0, 0], [10, 0], [20, 1], [25, 1], [25.1, 0]])
-total_schedules.append([[0, 1], [10, 1 - hmax], [20, 1 - hmax], [25, 1], [25.1, 1]])
+#h_schedules.append([[0, 0], [10, 0], [20, 1], [30, 1], [30.01, 0]])
+#total_schedules.append([[0, 1], [10, 1 - hmax], [20, 1 - hmax], [30, 1], [30.01, 1]])
+total_schedules.append([[0, 1], [30, 1]])
+h_schedules.append([[0, 0], [30, 0]])
 
 
 
 for n in [nval]:#range(3,19):
 
     explog = {
-        'name': 'new_pegasus_scaleh',
+        'name': 'new_internal_reset',
         'num_samples': num_samples * 10,
         'anneal_lenght': anneal_lenght,
         'N': n ** 2,
@@ -44,7 +46,7 @@ for n in [nval]:#range(3,19):
     }
 
     for k in hvals:
-        J = {link: -0.08 for link in qpu_sampler.edgelist}
+        J = {link: 0 for link in qpu_sampler.edgelist} #-0.2
         h = {node: -k for node in qpu_sampler.nodelist}
         bqm = dimod.BinaryQuadraticModel.from_ising(h, J)
 
@@ -53,7 +55,8 @@ for n in [nval]:#range(3,19):
         anneal_schedule = total_schedules[0]
         h_gain_schedule = h_schedules[0]
         for i in range(num_samples):
-            initial_config = 2*np.random.randint(2, size=n) - 1
+            #initial_config = 2*np.random.randint(2, size=n) - 1
+            initial_config = np.ones(n)
             initial_state = {qpu_sampler.properties["qubits"][i]: initial_config[i]
                             for i in range(len(qpu_sampler.properties["qubits"]))}
             samples = qpu_sampler.sample(bqm, initial_state=initial_state,
